2983f100b45f65769242e0d324ad0eb82a595851
Jose Silva <me@jsi1v4.dev>
2021-05-16 00:28:49 -0300

    Updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
 /* ### Global ### */
-var BRANCH = 'master';
+var BRANCH = 'main';
 var PROTOCOL = window.location.protocol;
 var PAGELEN = document.querySelectorAll('[id^="page"]').length - 1;
 var PAGE = 0;
