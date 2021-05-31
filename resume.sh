41128c5df15d39da1254ac4d0bb39371e7d63d4c
Jose Silva <me@jsi1v4.dev>
2021-05-31 20:16:58 -0300

    Updates

---
+++ resume.sh
@@ -0,0 +1,6 @@
+## show logo
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/jsi1v4/jsi1v4/main/assets/logo.sh)"
+## show resume
+echo -e "$(curl -fsSL https://raw.githubusercontent.com/jsi1v4/jsi1v4/main/topics/resume.md)\n"
+## show links
+echo -e "$(curl -fsSL https://raw.githubusercontent.com/jsi1v4/jsi1v4/main/topics/links.md)\n"
