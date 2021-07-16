77be90fd3c81f016a034ab0e5ec7c7d5c1e6f4a9
jsi1v4 <git@jsi1v4.com>
2021-07-15 22:51:17 -0300

    Updates

--- welcome.sh
+++ welcome.sh
@@ -1,6 +1,6 @@
 ## show logo
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/jsi1v4/jsi1v4/main/assets/logo.sh)"
-echo -e "I'm Jose Silva...\n"
+echo -e "I'm José Paulo (like 'Joseph')...\n"
 ## show resume
 echo -e "$(curl -fsSL https://raw.githubusercontent.com/jsi1v4/jsi1v4/main/topics/resume.md)\n"
 ## show links
